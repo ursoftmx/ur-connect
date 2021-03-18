1cab53a43cdd4c809ec463ff4411aafff2af488e
Alex Toga <aotech10@gmail.com>
2021-03-18 16:51:35 -0600

    feat: update product

--- lib/endpoints/class-wp-rest-articles-controller.php
+++ lib/endpoints/class-wp-rest-articles-controller.php
@@ -156,11 +156,130 @@ class WP_REST_Articles_Controller extends WP_REST_Controller
    */
   public function update_item($request)
   {
+    $authorization = $request->get_header('authorization');
+    $host =  $request->get_header('host');
     $id = $request->get_param('id');
-    $body =  array_diff_key($request->get_params(), array_flip(["id"]));
-    // TODO:  hacer el llamado del endpoint para actualizar
-    var_dump($id, $body);
-    exit;
+    $data =  array_diff_key($request->get_params(), array_flip(["id"]));
+    $segment =  "products/$id";
+    $result = [];
+    $data['meta_data'] = null;
+
+    if (isset($data['categories'])) {
+      $ids =  explode(',', trim($data['categories']));
+      $temp = [];
+      foreach ($ids as $id) {
+        $temp[] = [
+          'id' => $id,
+        ];
+      }
+      $data['categories'] = $temp;
+    }
+
+    if (isset($data['images'])) {
+      $imgs =  explode('|', trim($data['images']));
+      $temp = [];
+      foreach ($imgs as $index => $img) {
+        $temp[] = [
+          'src' => $img,
+          'position' => $index
+        ];
+      }
+      $data['images'] = $temp;
+    }
+
+    if (isset($data['enable_role_based_price'])) {
+      $data['meta_data'][] = [
+        'key' => '_enable_role_based_price',
+        'value' => $data['enable_role_based_price']
+      ];
+      unset($data['enable_role_based_price']);
+    }
+
+    if (isset($data['role_based_price'])) {
+
+      $roles_price = explode('|', trim($data['role_based_price']));
+      $values = [];
+
+      foreach ($roles_price as $role_price) {
+        $part = explode(':', $role_price);
+        $key = $part[0];
+        $value = $part[1];
+        $prices =  explode('_', $value);
+        $values[$key] = [
+          'regular_price' =>  isset($prices[0]) ? $prices[0] : '',
+          'selling_price' => isset($prices[1]) ? $prices[1] : '',
+        ];
+      }
+
+      $data['meta_data'][] = [
+        'key' => '_role_based_price',
+        'value' => $values
+      ];
+
+      unset($data['role_based_price']);
+    }
+
+    $data = json_encode($data);
+
+
+
+    $url = "https://$host/wp-json/wc/v2/$segment";
+
+    $headers = array(
+      'Content-Type' => 'application/json; charset=utf-8',
+      'Authorization' => $authorization,
+    );
+
+    $response = wp_remote_request($url, array(
+      'method'  => 'PUT',
+      'headers' => $headers,
+      'body'    => $data,
+      'data_format' => 'body',
+    ));
+
+    $status = array(
+      'success' => true,
+      'message' => 'message',
+      'code' => 'code'
+    );
+
+    if (is_wp_error($response)) {
+      // errr
+      $status = [
+        'debug' => $data,
+        'success' => false,
+        'message' => $response->get_error_message()
+      ];
+
+      $result = null;
+    } else {
+
+      // success
+      $status = [
+        'success' => true,
+        'message' => 'Updating product'
+      ];
+
+      $res = json_decode(wp_remote_retrieve_body($response));
+
+
+      if (isset($res->code)) {
+
+        $status = [
+          'success' => false,
+          'message' => $res->code
+        ];
+
+        $result = null;
+      } else {
+
+        $result = $res;
+      }
+    }
+
+    $data = $this->prepare_response_for_collecction($status, $result);
+
+    return new WP_REST_Response($data, 200);
   }
 
   /**
